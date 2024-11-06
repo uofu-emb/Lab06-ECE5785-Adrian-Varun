d4455733cd5e7a966b5e84dee0d832760f36f2af
JTAG <jtag@debian12.housing.utah.edu>
2024-11-06 13:52:30 -0700

    Created Activity 0

--- test/helper.c
+++ test/helper.c
@@ -3,29 +3,64 @@
 #include <task.h>
 #include "helper.h"
 
+#define TEST_DURATION       5000
 #define TASK_STACK_SIZE     2048
 
 void task_manager (
+        int num_tasks,
         TaskFunction_t pri_task, int pri_task_priority, int* pri_task_stats, int pri_delay,
         TaskFunction_t sec_task, int sec_task_priority, int* sec_task_stats, int sec_delay,
+        TaskFunction_t ter_task, int ter_task_priority, int* ter_task_stats, int ter_delay,
         int* total_task_stats, TickType_t* total_ticks
         )
 {   
-    TaskHandle_t pri_task_handle, sec_task_handle;
+    TaskHandle_t pri_task_handle, sec_task_handle, ter_task_handle;
 
+    if(num_tasks >= 1)
+    {
     // Start the primary task
-    vTaskDelay(pri_delay);
+    printf("Start task 1\n");
+    vTaskDelay(pdMS_TO_TICKS(pri_delay));
     xTaskCreate(pri_task, "Primary Thread",
                 TASK_STACK_SIZE, NULL, pri_task_priority, &pri_task_handle);
-
+    }
+    if(num_tasks >= 2)
+    {
     // Start the secondary task
-    vTaskDelay(sec_delay);
+    printf("Start task 2\n");
+    vTaskDelay(pdMS_TO_TICKS(sec_delay));
     xTaskCreate(sec_task, "Secondary Thread",
                 TASK_STACK_SIZE, NULL, sec_task_priority, &sec_task_handle);
+    }
+    if(num_tasks >= 3)
+    {
+        // Start the secondary task
+        printf("Start task 3\n");
+        vTaskDelay(pdMS_TO_TICKS(ter_delay));
+        xTaskCreate(ter_task, "Tertiary Thread",
+                TASK_STACK_SIZE, NULL, ter_task_priority, &ter_task_handle);
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(TEST_DURATION));
 
     // Get the statistics
-    *pri_task_stats = ulTaskGetRunTimeCounter(pri_task);
-
-    vTaskDelete(pri_task_handle);
-    vTaskDelete(sec_task_handle);
+    *total_task_stats = 0;
+    if(num_tasks >= 1)
+    {
+        *pri_task_stats = ulTaskGetRunTimeCounter(pri_task_handle);
+        *total_task_stats += *pri_task_stats;
+        vTaskDelete(pri_task_handle);
+    }
+    if(num_tasks >= 2)
+    {
+        *sec_task_stats = ulTaskGetRunTimeCounter(sec_task_handle);
+        *total_task_stats += *sec_task_stats;
+        vTaskDelete(sec_task_handle);
+    }
+    if(num_tasks >= 3)
+    {
+        *ter_task_stats = ulTaskGetRunTimeCounter(ter_task_handle);
+        *total_task_stats += *ter_task_stats;
+        vTaskDelete(ter_task_handle);
+    }
 }
--- test/test.c
+++ test/test.c
@@ -14,20 +14,87 @@
 
 #define TASK_STACK_SIZE 2048
 
+SemaphoreHandle_t semaphore;
+
+//=======================================
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
+//=======================================
+
+void inv_task_a(void* params)
+{
+    while(1) {
+    printf("Task A Waiting\n");
+    xSemaphoreTake(semaphore, portMAX_DELAY);
+    printf("Task A Busy\n");
+    busy_wait_us(1000000);
+    printf("Gave A Busy\n");
+    xSemaphoreGive(semaphore);
+    }
+}
+
+void inv_task_b(void* params)
+{
+    while(1)
+    {
+        printf("Task B Busy\n");
+        busy_wait_us(100000);
+    }
+}
+
+void inv_task_c(void* params)
+{
+    while(1) {
+    printf("Task C Waiting\n");
+    xSemaphoreTake(semaphore, portMAX_DELAY);
+    printf("Task C Busy\n");
+    busy_wait_us(100000);
+    printf("Gave C Busy\n");
+    xSemaphoreGive(semaphore);
+    }
+}
+
 void inversion_test()
 {
-    //Sample Test, Not Actual Code
-    TEST_ASSERT_EQUAL_INT(1, 1);
+    printf("Inversion test\n");
+    semaphore = xSemaphoreCreateBinary();
+    xSemaphoreGive(semaphore);
+
+    // Task A = Low Priority
+    // Task B = Med Priority
+    // Task C = High Priority    
+    const int A_prior = 3;
+    const int B_prior = 4;
+    const int C_prior = 5;
+
+    int A_stats, B_stats, C_stats, total_stats;
+
+    task_manager(3, 
+                inv_task_a, A_prior, &A_stats, 1, 
+                inv_task_c, C_prior, &C_stats, 1, 
+                inv_task_b, B_prior, &B_stats, 1, 
+                &total_stats, NULL);
+    
+    printf("A: %i, B: %i, C: %i, total: %i \n", A_stats, B_stats, C_stats, total_stats);
+
+    TEST_ASSERT(A_stats < 2000);
+    TEST_ASSERT(B_stats > 2000);
+    TEST_ASSERT(C_stats < 2000);
+
+    vSemaphoreDelete(semaphore);
 }
 
 void monitor_task(__unused void* params)
 {
-    RUN_TEST(inversion_test);
+    while (1)
+    {
+        RUN_TEST(inversion_test);
+
+        vTaskDelay(1000);
+    }
 }
 
 int main (void)
@@ -38,7 +105,7 @@ int main (void)
     UNITY_BEGIN();
     TaskHandle_t task;
     xTaskCreate(monitor_task, "MonitorThread",
-                TASK_STACK_SIZE, NULL, 0, &task);
+                TASK_STACK_SIZE, NULL, 20, &task);
     vTaskStartScheduler();
     sleep_ms(5000);
     return UNITY_END();
