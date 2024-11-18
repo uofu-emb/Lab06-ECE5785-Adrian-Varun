fdb56f18fbeb6cff7269db8930ce7b4d05ef853c
VarunBhavya <varu.varun1995@gmail.com>
2024-11-18 16:42:45 -0700

    Activity 2_1 completed

--- test/test.c
+++ test/test.c
@@ -24,6 +24,27 @@ void tearDown(void) {}
 
 //=======================================
 
+
+void busy_busy(void* params)
+{
+    for (int i = 0; ; i++);
+}
+
+void busy_yield(void* params)
+{
+    for (int i = 0; ; i++) {
+        taskYIELD();
+    }
+}
+
+void dummy_task(void* params){
+    /** Since our runner function takes 3 arguments to run,
+     * we define this dummy function to pass through but this function never executes.
+     * We use number of tasks to be run as an input first before creating tasks.
+     */
+}
+
+
 void inv_task_a(void* params)
 {
     while(1) {
@@ -117,12 +138,26 @@ void activity_1()
     vSemaphoreDelete(semaphore);
 }
 
+void activity_run_busy_busy(){
+    int priority_level = tskIDLE_PRIORITY+(3);
+    int A_stats, B_stats, dummy_stats, total_stats;
+    task_manager(2, 
+                busy_busy, priority_level, &A_stats, 1, 
+                busy_busy, priority_level, &B_stats, 1, 
+                dummy_task, priority_level, &dummy_stats, 1, 
+                &total_stats, NULL);
+    printf("A: %i, B: %i, total: %i \n", A_stats, B_stats, total_stats);
+    TEST_ASSERT(A_stats > 2000000);
+    TEST_ASSERT(B_stats > 2000000);
+}
+
 void monitor_task(__unused void* params)
 {
     while (1)
     {
         RUN_TEST(activity_0);
         RUN_TEST(activity_1);
+        RUN_TEST(activity_run_busy_busy);
         vTaskDelay(1000);
     }
 }
